67ed4084c93160cca3a1d4090f2021704065f84a
Jordan <60279731+jordnance@users.noreply.github.com>
2021-11-24 14:35:04 -0800

    Updated rebuild.sql

--- SQL Scripts/rebuild.sql
+++ SQL Scripts/rebuild.sql
@@ -1,7 +1,10 @@
 source drop_tables.sql;
 source drop_views.sql;
+source drop_stored_procedures.sql;
 source create_tables.sql;
 source create_views.sql;
+source addUser.sql;
+source deleteUser.sql;
 
 source input/user_input.sql;
 source input/adult_input.sql;
