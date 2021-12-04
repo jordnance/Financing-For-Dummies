fa76cf750d25a11f4f51e47872271f5bf514d876
jordnance <jcox@artemis.cs.csubak.edu>
2021-12-04 15:46:04 -0800

    Minor navigation button edit

--- settings.php
+++ settings.php
@@ -18,7 +18,7 @@ require_once "config.php";
         <li><a class="button" href="home.php">Home</a></li>
         <li><a class="button" href="accountInterface.php">Accounts</a></li>
         <li><a class="button" href="analytics.php">Analytics</a></li>
-        <li><a class="button" href="userRoles.php">User Roles</a></li>
+        <li><a class="button" href="">User Roles</a></li>
         <li><a class="button" href="newTransaction.php">New Transaction</a></li>
         <li><a class="button" href="#top">Settings</a></li>
         <li><button class="link" form="logout" name="logout">Log Out</button></li>
